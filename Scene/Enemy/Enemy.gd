d1aa4f620320ead1e18e82fb5c101c3057b78e4e
Zheng ann Liew <zhliew@u91z05s03.42kl.edu.my>
2023-11-24 22:14:34 +0800

    .

--- Scene/Enemy/Enemy.gd
+++ Scene/Enemy/Enemy.gd
@@ -6,38 +6,26 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 @onready var nav_agent = $NavigationAgent2D
-@onready var raycast_up = $RayCast2DUp
-#@onready var raycast_up = $ShapeCast2D
-@onready var raycast_down = $RayCast2DDown
 
-var bullet = preload("res://Scene/Bullet/HGBullet.tscn")
-var can_fire = true
-var fire_rate : float = 1
-var in_range : bool = false
 var target: CharacterBody2D
+var is_chasing : bool = false
 
 func _ready():
 	await get_tree().process_frame
 	target = get_tree().get_nodes_in_group("Player")[0]
+	target.connect("playerspotted", handle_player_spotted)
 	make_path()
 
 func _physics_process(delta):
 	velocity = Vector2.ZERO
 	target = get_tree().get_nodes_in_group("Player")[0]
 	look_at(target.global_position)
-	#rotation = velocity.angle()
-	if (raycast_up.is_colliding() and raycast_up.get_collider().is_in_group("Player")):
-		#and raycast_down.is_colliding() and raycast_down.get_collider().is_in_group("Player")):
-		in_range = true
-		if (can_fire == true):
-			fire()
-	if (in_range == false):
+
+	if (is_chasing):
 		var direction = global_position.direction_to(nav_agent.get_next_path_position())
 		velocity = direction * move_speed
 		move_and_slide()
 	pick_new_state()
-	in_range = false
-
 
 func make_path():
 	nav_agent.target_position = target.global_position
@@ -51,11 +39,9 @@ func pick_new_state():
 func _on_timer_timeout():
 	make_path()
 	
-func fire():
-	var bullet_instance = bullet.instantiate()
-	bullet_instance.position = $BulletSpawnPoint.global_position
-	bullet_instance.target = target.position
-	get_tree().get_root().add_child(bullet_instance)
-	can_fire = false
-	await(get_tree().create_timer(fire_rate).timeout)
-	can_fire = true
+func handle_player_spotted():
+	if (is_chasing == false):
+		is_chasing = true
+	elif (is_chasing == true):
+		is_chasing = false
+	
--- Scene/Player/Player.gd
+++ Scene/Player/Player.gd
@@ -2,21 +2,24 @@ extends CharacterBody2D
 
 @export var move_speed: float = 150
 
+@onready var tourch_light = $PointLight2D
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 
-var bullet = preload("res://Scene/Bullet/HGBullet.tscn")
 var can_fire = true
 var fire_rate : float = 1
+
+signal playerspotted
 
 func _physics_process(delta):
 	
 #	rotate(get_angle_to(get_global_mouse_position()))
 	look_at(get_global_mouse_position())
 	
-	if (Input.is_action_pressed("left_click") and can_fire):
-		fire()
-	
+#	if (Input.is_action_pressed("left_click") and can_fire):
+#		fire()
+	if (Input.is_action_just_pressed("SpaceBar")):
+		switch_torch_light()
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up"),
@@ -27,17 +30,22 @@ func _physics_process(delta):
 	pick_new_state()
 	
 func pick_new_state():
-	
 	if (velocity != Vector2.ZERO):
 		state_machine.travel("Walk")
 	else:
 		state_machine.travel("Idle")
 		
-func fire():
-	var bullet_instance = bullet.instantiate()
-	bullet_instance.position = $BulletSpawnPoint.global_position
-	bullet_instance.target = get_global_mouse_position()
-	get_tree().get_root().add_child(bullet_instance)
-	can_fire = false
-	await(get_tree().create_timer(fire_rate).timeout)
-	can_fire = true
+	
+func switch_torch_light():
+	emit_signal("playerspotted")
+	if (tourch_light.enabled == true):
+		tourch_light.enabled = false
+	elif(tourch_light.enabled == false):
+		tourch_light.enabled = true
+		
+		
+		
+		
+		
+		
+		
